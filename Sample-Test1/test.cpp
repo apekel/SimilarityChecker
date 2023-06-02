b2e0c2200732185f3b3e9fafb10ab3c3aafd012a
apekel <jong3432@gmail.com>
2023-06-02 11:44:21 +0900

    글자 수 check

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,34 @@
 #include "pch.h"
 #include "../SimilarityChecker/SimilarityChecker.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(CheckCount, Same)
+{
+	SimilarityChecker similarityChecker;
+	string input1 = "ABC";
+	string input2 = "ABC";
+	EXPECT_EQ(60, similarityChecker.CheckCount(input1, input2));
+}
+
+TEST(CheckCount, Diff1)
+{
+	SimilarityChecker similarityChecker;
+	string input1 = "ABC";
+	string input2 = "ABCABC";
+	EXPECT_EQ(0, similarityChecker.CheckCount(input1, input2));
+}
+
+TEST(CheckCount, Diff2)
+{
+	SimilarityChecker similarityChecker;
+	string input1 = "ABCAB";
+	string input2 = "ABC";
+	EXPECT_EQ((1 - (5-3)/3)*60, similarityChecker.CheckCount(input1, input2));
+}
+
+TEST(CheckCount, Diff3)
+{
+	SimilarityChecker similarityChecker;
+	string input1 = "ABC";
+	string input2 = "ABCAB";
+	EXPECT_EQ((1 - (5 - 3) / 3) * 60, similarityChecker.CheckCount(input1, input2));
 }
--- SimilarityChecker/SimilarityChecker.cpp
+++ SimilarityChecker/SimilarityChecker.cpp
@@ -1,4 +1,29 @@
+#include <string>
+using namespace std;
+
 class SimilarityChecker
 {
-	
+public:
+	int CheckCount(const string& input1, const std::string& input2)
+	{
+		if (input1.length() == input2.length())
+		{
+			return 60;
+		}
+
+		if (input1.length() >= input2.length() * 2 ||
+			input2.length() >= input1.length() * 2)
+		{
+			return 0;
+		}
+
+		if (input1.length() > input2.length())
+		{
+			return (1 - (input1.length() - input2.length()) / input2.length()) * 60;
+		}
+		else
+		{
+			return (1 - (input2.length() - input1.length()) / input1.length()) * 60;
+		}
+	}
 };
