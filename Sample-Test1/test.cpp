7677c56219d8139dcb4d0cd1c57a8cb9f39164b1
apekel <jong3432@gmail.com>
2023-06-02 13:32:58 +0900

    alphabet check

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -32,3 +32,34 @@ TEST(CheckCount, DiffInput2Long)
 	string input2 = "ABCAB";
 	EXPECT_EQ(((2 * 3 - 5) * 60) / 3, similarityChecker.CheckCount(input1, input2));
 }
+TEST(CheckAlphabet, Same1)
+{
+	SimilarityChecker similarityChecker;
+	string input1 = "ABC";
+	string input2 = "CBA";
+	EXPECT_EQ(40, similarityChecker.CheckAlphabet(input1, input2));
+}
+
+TEST(CheckAlphabet, Same2)
+{
+	SimilarityChecker similarityChecker;
+	string input1 = "ABC";
+	string input2 = "ABCAB";
+	EXPECT_EQ(40, similarityChecker.CheckAlphabet(input1, input2));
+}
+
+TEST(CheckAlphabet, Diff1)
+{
+	SimilarityChecker similarityChecker;
+	string input1 = "ABC";
+	string input2 = "DEF";
+	EXPECT_EQ(0, similarityChecker.CheckAlphabet(input1, input2));
+}
+
+TEST(CheckAlphabet, Diff2)
+{
+	SimilarityChecker similarityChecker;
+	string input1 = "ABC";
+	string input2 = "AAAAA";
+	EXPECT_EQ(1/3*40, similarityChecker.CheckAlphabet(input1, input2));
+}
